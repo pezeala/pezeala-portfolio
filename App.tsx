14753c25467f26c43c85086f9bb45ee0784f9c9e
pezeala <pcezeala@gmail.com>
2025-11-28 15:39:28 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Introduction from './components/Introduction';
 import About from './components/About';
 import Experience from './components/Experience';
 import Skills from './components/Skills';
@@ -16,6 +17,7 @@ const App: React.FC = () => {
       <Navbar />
       <main>
         <Hero />
+        <Introduction />
         <About />
         <Experience />
         <Skills />
